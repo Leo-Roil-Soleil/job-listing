b3066e81d9d02d7ae421ea771de93c1ff3a30de8
Leo-Roil-Soleil <leokexin@gmail.com>
2017-01-11 21:17:24 +0600

    implement step 5

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,14 @@ Rails.application.routes.draw do
   devise_for :users
   resources :jobs
   root 'jobs#index'
+
+  namespace :admin do
+     resources :jobs do
+       member do
+         post :publish
+         post :hide
+       end
+     end
+   end
+
 end
