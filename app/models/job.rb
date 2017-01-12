53b2ef25c00e2e56445f38d8fd16953c35f95637
Leo-Roil-Soleil <leokexin@gmail.com>
2017-01-12 08:33:41 +0600

    step 6 order by wage

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,11 +1,3 @@
 class Job < ApplicationRecord
-  def publish!
-    self.is_hidden = false
-    self.save
-  end
-
-  def hide!
-    self.is_hidden = true
-    self.save
-  end
-end  
+  scope :recent, -> { order('created_at DESC') }
+end
