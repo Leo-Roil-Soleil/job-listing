3ace3aeab47bdf60b241ebded8595a3d4ba61fcd
Leo-Roil-Soleil <leokexin@gmail.com>
2017-01-23 21:43:29 +0800

    step 7 upload resume

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -14,7 +14,7 @@ class ResumesController < ApplicationController
       @resume.user = current_user
 
       if @resume.save
-        flash[:notice] = "成功提交履历"
+        flash[:notice] = '成功提交履历'
         redirect_to job_path(@job)
       else
         render :new
